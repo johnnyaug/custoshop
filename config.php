4bf0d36cf713c86bdf3c60036fb99fc5f528192e
johnnyaug@gmail.com <johnnyaug@gmail.com>
2015-01-07 19:56:35 +0200

    first commit

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.custoshop.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.custoshop.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/yoni/Development/start-theme/catalog/');
+define('DIR_SYSTEM', '/home/yoni/Development/start-theme/system/');
+define('DIR_LANGUAGE', '/home/yoni/Development/start-theme/catalog/language/');
+define('DIR_TEMPLATE', '/home/yoni/Development/start-theme/catalog/view/theme/');
+define('DIR_CONFIG', '/home/yoni/Development/start-theme/system/config/');
+define('DIR_IMAGE', '/home/yoni/Development/start-theme/image/');
+define('DIR_CACHE', '/home/yoni/Development/start-theme/system/cache/');
+define('DIR_DOWNLOAD', '/home/yoni/Development/start-theme/system/download/');
+define('DIR_UPLOAD', '/home/yoni/Development/start-theme/system/upload/');
+define('DIR_MODIFICATION', '/home/yoni/Development/start-theme/system/modification/');
+define('DIR_LOGS', '/home/yoni/Development/start-theme/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'shop1987');
+define('DB_DATABASE', 'custoshop');
+define('DB_PREFIX', 'oc_');
